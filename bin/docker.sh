301a0541518c4219af3a4f5a55e461403b3cc956
Eric Wang <wjun0912@gmail.com>
2021-12-24 18:54:40 +1100

    ci: publish docker image on release (#51)

---
+++ bin/docker.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+get_image_name() {
+  pushd > /dev/null "${REPO_ROOT}"
+  local version
+  version=$(cat package.json | jq -r .version)
+  popd > /dev/null
+  echo "fa93hws/snowball-rss:${version}"
+}
+
+build_docker() {
+  pushd > /dev/null "${REPO_ROOT}"
+  local image_name
+  image_name=$(get_image_name)
+  echo "Building image ${image_name}"
+  docker build -t "${image_name}" .
+  popd > /dev/null
+}
+
+# $1 comand: build or publish
+main() {
+  local command="$1"
+  if [[ -z "${command}" ]]; then
+    echo "Usage: $0 <command>"
+    echo "  command: build or publish"
+    exit 1
+  fi
+  case "${command}" in
+    build)
+      build_docker
+      ;;
+    publish)
+      docker push $(get_image_name)
+      ;;
+    *)
+      echo "unknown command: ${command}, must be build or publish"
+      exit 1
+      ;;
+  esac
+}
+
+main "$@"
