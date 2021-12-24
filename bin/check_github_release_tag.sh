f6f9e583e498919ed289c55a9d64e0d029302dbd
Eric Wang <wjun0912@gmail.com>
2021-12-24 21:00:33 +1100

    ci: push to github docker registry (#58)

--- bin/check_github_release_tag.sh
+++ bin/check_github_release_tag.sh
@@ -4,20 +4,21 @@ set -o pipefail
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-source "${REPO_ROOT}/bin/_version.sh"
-
 # $1: github tag
 check_github_release_tag() {
   if [[ "$#" == 0 ]]; then
     echo "github tag must not be empty"
     exit 1
   fi
+  pushd "${REPO_ROOT}" >/dev/null
   local github_tag="$1"
-  local version=$(_get_version)
+  local version
+  version=$(cat package.json | jq -r .version)
   if [[ "${version}" != "${github_tag}" ]]; then
     echo "package version ${version} does not match github tag ${github_tag}"
     exit 1
   fi
+  popd > /dev/null
 }
 
 check_github_release_tag "$@"
