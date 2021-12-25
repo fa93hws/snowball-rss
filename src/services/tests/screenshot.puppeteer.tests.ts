21c160a07452e91bba85622bbe0b6c553b72ec53
eric wang <eric@canva.com>
2021-12-25 23:37:58 +1100

    feat: app to crash if puppeteer failed to launch

--- src/services/tests/screenshot.puppeteer.tests.ts
+++ src/services/tests/screenshot.puppeteer.tests.ts
@@ -1,11 +1,20 @@
-import { ScreenShotService } from '../screenshot-service';
-import { fakeLogger } from '../fake/logging-service';
+import type { ICrashService } from '@services/crash-service';
 import fs from 'fs';
 import path from 'path';
+import { ScreenShotService } from '../screenshot-service';
+import { fakeLogger } from '../fake/logging-service';
 
 describe('screenshot', () => {
   jest.setTimeout(60000);
-  const service = new ScreenShotService(fakeLogger);
+
+  const fakeCrashService: ICrashService = {
+    crash: jest.fn(),
+  };
+  const service = new ScreenShotService({
+    logger: fakeLogger,
+    crashService: fakeCrashService,
+  });
+
   it('take a screenshot for a given url', async () => {
     const url = 'https://xueqiu.com/1334706236/174333684';
     const bufferResult = await service.capturePage(url);
