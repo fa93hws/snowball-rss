7ecc96a743e0d4410f2ce66e5996a583f8b224e5
eric wang <eric@canva.com>
2022-01-03 12:51:31 +1100

    feat: keep by-qq only

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { qqCommand } from '../src/command/qq/qq-command';
-import { emailCommand, deparactedEmailCommand } from './command/email/email-command';
-import { slackCommand } from './command/slack/slack-command';
 
 export function main() {
   return yargs(hideBin(process.argv))
-    .command(emailCommand)
-    .command(deparactedEmailCommand)
-    .command(slackCommand)
     .command(qqCommand)
     .strict(true)
     .exitProcess(true)
